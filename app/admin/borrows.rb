0707638e74a0339de7b83c8eb7c2995ce60b6777
mahmoud-adel-inova-23 <mahmoud.adel.inovaeg@gmail.com>
2023-07-09 10:16:01 +0300

    refator switch case for admin borrow status

--- app/admin/borrows.rb
+++ app/admin/borrows.rb
@@ -10,8 +10,6 @@ ActiveAdmin.register Borrow do
 
   filter :book, collection: -> { Book.includes(:translations).all.map { |b| [b.name, b.id] } }
 
-
-
   action_item :approve, only: :show do
     link_to "Apprrove", approve_admin_borrow_path(borrow), method: :put if borrow.pending?
   end
@@ -38,34 +36,17 @@ ActiveAdmin.register Borrow do
     column :user
     column :book
     column('status') do |b|
-      case b.status
-      when 'pending'
-        status_tag b.status, class: 'status-pending'
-      when 'approved'
-        status_tag b.status, class: 'status-approved'
-      when 'rejected'
-        status_tag b.status, class: 'status-rejected'
-      else
-        status_tag b.status, class: 'status-rejected'
-      end
+      status_tag b.status, class: STATUS_CLASSES[b.status] || 'status-rejected'
     end
     column('Returned') { |b| status_tag b.returned_at? }
-    column('Returned At') { |b|  b.returned_at_formated }
+    column('Returned At') { |b| b.returned_at_formated }
     actions
   end
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :user_id, :book_id, :borrowed_at, :return_time, :returned_at, :status
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:user_id, :book_id, :borrowed_at, :return_time, :returned_at, :status]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
+
+  STATUS_CLASSES = {
+    'pending' => 'status-pending',
+    'approved' => 'status-approved',
+    'rejected' => 'status-rejected'
+  }.freeze
 
 end
